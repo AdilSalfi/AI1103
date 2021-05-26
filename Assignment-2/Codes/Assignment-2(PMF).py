3e87736271e2be4927772af400676ee0c6fe0601
AdilSalfi <80508076+AdilSalfi@users.noreply.github.com>
2021-05-26 20:10:31 +0530

    Update Assignment-2(PMF).py

--- Assignment-2/Codes/Assignment-2(PMF).py
+++ Assignment-2/Codes/Assignment-2(PMF).py
@@ -3,7 +3,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 plt.stem([0, 1, 2, round(11/3, 1)],[1/4, 1/12, 1/6, 1/2])
-plt.title("Discrete Probability distribution of Random Variable X")
+plt.title("PMF of Random Variable X")
 plt.xlabel('$X=x$')
 plt.ylabel("Probability")
 plt.xticks([0, 1, 2, round(11/3, 1)])
