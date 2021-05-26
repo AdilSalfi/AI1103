2759911531c76dcd26f431c2823d3bc19ce76e66
AdilSalfi <80508076+AdilSalfi@users.noreply.github.com>
2021-05-26 18:08:05 +0530

    Update Assignment-2.py

--- Assignment-2/Codes/Assignment-2.py
+++ Assignment-2/Codes/Assignment-2.py
@@ -1,1 +1,29 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from fractions import Fraction
 
+def F(x):
+  if(x<0):
+    return 0
+
+  elif(x>=0 and x<1):
+    return 1/4
+
+  elif(x>=1 and x<2):
+    return 1/3
+
+  elif(x>=2 and x<11/3):
+    return 1/2
+
+  elif(x>=11/3):
+    return 1
+
+xlist = np.linspace(-1,4,100000)
+ylist = [F(x) for x in xlist]
+
+plt.xticks([1, 0, 1, 2,3, round(11/3, 1), 4])
+plt.plot(xlist, ylist)
+plt.title("CDF of Random Variable X")
+plt.xlabel('$x$')
+plt.ylabel('$F_Xx$')
+plt.show()
